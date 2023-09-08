20925db78ee202231c1aaddf9818e3b44f4a2d12
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2023-09-08 20:22:41 -0300

    ...

---
+++ codes/bhaskara.py
@@ -0,0 +1,5 @@
+a = float(input("insira o primeiro valor:"))
+b = float(input("insira o segundo valor:"))
+c = (a ** 2 + b ** 2) ** 0.5
+
+print("O valor da hipotenusa é", c)
--- main.py
+++
@@ -1,5 +0,0 @@
-a = float(input("insira o primeiro valor:"))
-b = float(input("insira o segundo valor:"))
-c = (a ** 2 + b ** 2) ** 0.5
-
-print("O valor da hipotenusa é", c)
