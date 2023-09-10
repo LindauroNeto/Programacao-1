24e02bb7c9a5335c88f73c8229b5bf8093c61ba1
LindauroNeto <140420389+LindauroNeto@users.noreply.github.com>
2023-09-10 01:44:45 -0300

    Adivinhação.py e Enquanto(for).py
    Exercícios com o uso dos loops while e for.

---
+++ codes/adivinhação.py
@@ -0,0 +1,7 @@
+n = 0
+while n != 10:
+    n = float(input("Tente adivinhar um número: "))
+
+    if n != 10:
+        print("Não... esse não é o valor")
+print("Você descobriu! O valor era 10!")
---
+++ codes/enquanto(for).py
@@ -0,0 +1,5 @@
+u = int(input("Digite até quanto eu devo contar: "))
+
+for i in range(u):
+    print(i, "...")
+print("Pronto, eu contei de 0 até", u)
